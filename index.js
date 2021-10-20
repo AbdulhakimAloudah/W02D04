0da8fe7d38895e27150a0e87ec4fc25c8aa1c246
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-20 21:51:17 +0300

    wv

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// الفكرة
+// الفكرة// الفكرة
 // h2 سوف اقوم بانشاء عنوان
 //  document.createElement طريق
-// ul ثم اضيف
+// ul ثم اضيف القائمة
 //  ثم button
 //  ثم input
 
-//  سوف يتم الاضافة على القايمة عن طريق push
+//  .push سوف يتم الاضافة الى القايمة عن طريق
 //  button function لاضافة
 
 //الحذف من القائمة pop
@@ -16,26 +16,29 @@ const h1 = document.createElement("h1");
 h1.innerHTML = "Todo List";
 body.append(h1);
 
+
+//-------
 const ul = document.createElement("ul");
 ul.className = "uul";
-const ulSelector = document.querySelector(".uul");
-
+const ulSelector = document.querySelector("uul");
 body.append(ul);
 
 const toDos = ["wake up", "eat breakfast", "code"];
 
+
+//-----
 const input = document.createElement("input");
 input.type = "text";
 input.id = "myInput";
 body.append(input);
 
+//------
 const btn = document.createElement("button");
 btn.innerHTML = "Button";
 body.append(btn);
 
+//-------
 function renderList() {
-for (let i = 0; i < toDos.length; i++) {}
-ulSelector.append(toDos);
-
-
+  for (let i = 0; i < toDos.length; i++) {}
+  ulSelector.append(toDos);
 }
