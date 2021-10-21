df1421d02cad8a5857fd0af7a6b16f76d711a285
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-21 20:35:17 +0300

    edf

--- index.js
+++ index.js
@@ -1,44 +1,72 @@
-// الفكرة// الفكرة
-// h2 سوف اقوم بانشاء عنوان
-//  document.createElement طريق
-// ul ثم اضيف القائمة
-//  ثم button
-//  ثم input
+const newele = document.querySelector("body");
+const gtt = document.createElement("h1");
 
-//  .push سوف يتم الاضافة الى القايمة عن طريق
-//  button function لاضافة
+gtt.innerHTML = "To DO list";
+newele.append(gtt);
+gtt.className = "text-center";
 
-//الحذف من القائمة pop
-///-----------------------------------
+const drgd = document.createElement("ul");
+drgd.id = "swwer";
+newele.append(drgd);
 
-const body = document.querySelector("body");
-const h1 = document.createElement("h1");
-h1.innerHTML = "Todo List";
-body.append(h1);
+let todos = ["wakeup", "eat breack fast", "code"];
 
+const sul = document.querySelector("ul");
+const deleteFun = (i) => {
+  todos = todos.filter((task, index) => index !== i);
+  renderList();
+};
 
-//-------
-const ul = document.createElement("ul");
-ul.className = "uul";
-const ulSelector = document.querySelector("uul");
-body.append(ul);
-
-const toDos = ["wake up", "eat breakfast", "code"];
-
-
-//-----
-const input = document.createElement("input");
-input.type = "text";
-input.id = "myInput";
-body.append(input);
-
-//------
-const btn = document.createElement("button");
-btn.innerHTML = "Button";
-body.append(btn);
-
-//-------
-function renderList() {
-  for (let i = 0; i < toDos.length; i++) {}
-  ulSelector.append(toDos);
+function update1(i) {
+  const kk = window.prompt("change value");
+  todos[i.currentTarget.rer] = eaer;
+  console.log(todos);
+  renderList();
 }
+
+const renderList = () => {
+  drgd.innerHTML = "";
+  todos.forEach((element, index) => {
+    const ewrg = document.createElement("li");
+    ewrg.innerHTML = element;
+    sul.append(ewrg);
+    const rr = document.createElement("button");
+    rr.innerHTML = "remove";
+    sul.append(rr);
+    rr.addEventListener("click", () => deleteFun(index));
+    const update = document.createElement("button");
+    update.innerHTML = "update";
+    sul.append(update);
+    update.rer = index;
+    update.addEventListener("click", update1);
+    rr.className = "btn-danger";
+    update.className = "btn-success";
+    ewrg.className = "active";
+    sul.className = "row-xs-30";
+    ewrg.className = "form-control";
+    ewrg.className = "center-block";
+  });
+};
+
+renderList();
+
+const newinput = document.createElement("input");
+newinput.className = "bb";
+newele.append(newinput);
+
+const newbuttom = document.createElement("button");
+newbuttom.className = "btn-info";
+newbuttom.innerHTML = "click here";
+newele.append(newbuttom);
+newele.className = "btn-warning";
+
+const o9 = document.querySelector("#su");
+const bb = document.querySelector("#bb");
+
+o9.addEventListener("click", () => {
+  if (newinput.value.length) {
+    todos.push(newinput.value);
+    newinput.value = "";
+    renderList();
+  }
+});
